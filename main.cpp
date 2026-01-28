451a0428f88a37570f50c6dd53d59a198907051a
Candy <swastiktoprani@gmail.com>
2026-01-28 12:07:40 +0000

    Need tto make a socket and work on connecting it after using acceptor

--- main.cpp
+++ main.cpp
@@ -9,21 +9,23 @@ int main()
     //TODO : Figure out, how to work with error codes in Boost::asio
     try {
         //Step -1 Create I/O context(Think of this as "I/O Engine")
-        boost::asio::io_service io_service; //basically hides all the boost specific things
+        boost::asio::io_context io_context; //basically hides all the boost specific things
 
         //Step-3 Create an endpoint by basically giving a string to get converted into something it understands//
         unsigned short PORT = 5000;
         tcp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), PORT);
 
         //  //Create a socket here //
-        // tcp::socket socket(io_service);
+        // tcp::socket socket(io_context);
         //
         //  //Step-5 ask the socket to try and connect to someting
         //  socket.connect()
         //
 
         //Step-5 Create a acceptor I dont know what this  but I need to bound to that enddpoint
-        tcp::acceptor acceptor(io_service , endpoint);
+        tcp::acceptor acceptor(io_context , endpoint);
+
+        tcp::socket socket(io_context);
 
         std::cout<<"Server Bound to 127.0.0.1: "<<PORT<<std::endl;
 
