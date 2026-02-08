90191c9b1d1e846c0f85b9f34938b46d23f1cbf3
Candy <swastiktoprani@gmail.com>
2026-02-08 12:29:19 +0000

    Moved Main into src folder , created an src folder . And made sure Cmake makes two different exec for main and client

---
+++ src/Client.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Candy on 2/7/26.
+//
+
+#include<iostream>
+#include<array>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+void HandleConnection (boost::asio::ip::tcp::socket& socket)
+{
+
+    std::string message = "HELLO TO YOUR MOTHER\n";
+    std::cout<<message<<std::endl;
+
+    boost::asio::write(socket, boost::asio::buffer(message));
+    std::cout<<"The client sent the message"<<std::endl;
+
+    //Creating buffer to read what the server echo's back//
+    std::array<char , 1024>buffer{};
+
+    //Creating a block and waiting for the server to send some bytes//
+    std::size_t bytes_read = socket.read_some(boost::asio::buffer(buffer));
+
+    std::string ServerMessage(buffer.data(), bytes_read);
+    std::cout<<"The client received the message"<<std::endl;
+
+    std::cout << "The client received the message: "
+              << ServerMessage << std::endl;
+
+}
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+        unsigned short int PORT = 5000;
+
+        tcp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"),PORT);
+
+        //Create a ssocket
+
+        tcp::socket socket(io_context);
+
+        socket.connect(endpoint);
+
+HandleConnection(socket);
+
+    }catch(std::exception& e)
+    {
+        std::cout<<e.what()<<std::endl;
+
+    }
+
+return 1;
+}
