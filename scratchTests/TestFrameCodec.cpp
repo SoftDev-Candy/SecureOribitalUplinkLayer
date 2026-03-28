71e9f0c4589c8f5ebf6bd88bed26f66ed852725d
Candy <swastiktoprani@gmail.com>
2026-03-28 12:59:00 +0000

    Test works for Encoding and conversion from Json and tojson .

--- scratchTests/TestFrameCodec.cpp
+++ scratchTests/TestFrameCodec.cpp
@@ -1,31 +1,50 @@
-//
-// Created by Candy on 3/18/26.
-//
+    //
+    // Created by Candy on 3/18/26.
+    //
 
-//FIXME -- File here only for me to test the code nothing else nothing more //
+    //FIXME -- File here only for me to test the code nothing else nothing more //
 
-#include <iostream>
-#include "../Common/FrameCodec.hpp"
-#include "../Common/TelemetryFrame.hpp"
+    #include <iostream>
+    #include "../Common/FrameCodec.hpp"
+    #include "../Common/TelemetryFrame.hpp"
 
-int main()
-{
-    TelemetryFrame tf;
-    tf.sat_id = "SAT_1";
-    tf.sequence = 482094824;
-    tf.timestamp_ms = 749284738947;
-    tf.battery =89.3;
-    tf.temp_c = 44.6;
+    int main()
+    {
+        TelemetryFrame tf;
+        tf.sat_id = "SAT_1";
+        tf.sequence = 482094824;
+        tf.timestamp_ms = 749284738947;
+        tf.battery =89.3;
+        tf.temp_c = 44.6;
 
-    std::string tojsontest1= tf.ToJson();
-    std::printf("%s\n", tojsontest1.c_str());
+        std::string tojsontest1= tf.ToJson();
+        std::printf("%s\n", tojsontest1.c_str());
 
-    tf.FromJson(tojsontest1);
-    std::printf("%s\n", tojsontest1.c_str());
+        auto encode = tf.FromJson(tojsontest1);
+        if (encode)
+        {
 
-    FrameCodec frame;
-    frame.EncodeFrame(tojsontest1);
-    std::printf("%s\n", tojsontest1.c_str());
+            std::cout<<encode->sat_id<<"\n";
+            std::cout<<encode->sequence<<"\n";
+            std::cout<<encode->timestamp_ms<<"\n";
+            std::cout<<encode->battery<<"\n";
+            std::cout<<encode->temp_c<<"\n";
 
-    return 0;
-}
+        }
+
+        FrameCodec frame;
+        auto encoded = frame.EncodeFrame(tojsontest1);
+        auto json_size = static_cast<uint32_t>(tojsontest1.size());
+        std::cout<<"Size of JSON is :"<<json_size<<"\n";
+        std::cout<<"Size of Encode_JSON is :"<<encoded.size()<<"\n";
+
+        if (encoded.size() == 4 + json_size)
+        {
+            for (size_t i = 0 ; i < 4 ; i++)
+            {
+                std::cout<<static_cast<int>(encoded[i])<<" ";
+            }
+        }
+
+        return 0;
+    }
