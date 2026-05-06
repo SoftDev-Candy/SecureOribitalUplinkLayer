73383a1980b90142e3a7ca7352243a8e5450bbed
SoftDev-Candy <swastiktoprani@gmail.com>
2026-05-06 12:55:59 +0200

    Improved shader using the game engine code .Added

--- OrbitalRendering/Shader/ShaderSource.hpp
+++ OrbitalRendering/Shader/ShaderSource.hpp
@@ -15,9 +15,22 @@ const char* VertexShader()
         layout(location = 2) in vec3 aNormal;
 
         uniform mat4 uMVP;
+        uniform mat4 uModel;
+        uniform mat3 uNormalMatrix;
+
+        out vec2 vUV;
+        out vec3 vWorldPos;
+        out vec3 vNormal;
 
         void main()
         {
+            vUV = aUV;
+
+            vec4 worldPos = uModel * vec4(aPos, 1.0);
+            vWorldPos = worldPos.xyz;
+
+            vNormal = normalize(uNormalMatrix * aNormal);
+
             gl_Position = uMVP * vec4(aPos, 1.0);
         }
     )";
@@ -28,11 +41,32 @@ const char* FragmentShader()
     return R"(
         #version 330 core
 
+        in vec2 vUV;
+        in vec3 vWorldPos;
+        in vec3 vNormal;
+
         out vec4 FragColor;
+
+        uniform sampler2D uDayMap;
+        uniform sampler2D uNightMap;
+
+        uniform vec3 uSunDir;
+        uniform vec3 uViewPos;
 
         void main()
         {
-            FragColor = vec4(0.1, 0.35, 1.0, 1.0);
+            vec3 N = normalize(vNormal);
+            vec3 L = normalize(-uSunDir);
+
+            float dayAmount = dot(N, L);
+            dayAmount = smoothstep(-0.15, 0.25, dayAmount);
+
+            vec3 dayColor = texture(uDayMap, vUV).rgb;
+            vec3 nightColor = texture(uNightMap, vUV).rgb;
+
+            vec3 earthColor = mix(nightColor, dayColor, dayAmount);
+
+            FragColor = vec4(earthColor, 1.0);
         }
     )";
 }
